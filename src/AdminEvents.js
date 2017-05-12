01ffa24c036ccbff3f3e35284a6db974b0bf066c
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-05-12 17:03:13 +0200

    Csináltam valamiket

--- src/AdminEvents.js
+++ src/AdminEvents.js
@@ -45,17 +45,12 @@ class AdminEvents extends Component {
     });
   }
 
-  getNewEvents() {
-
-  }
-
   render() {
     console.log('this.state: ', this.state);
     console.log('this.props.titles: ', this.props.titles);
     return (
        <section id="admin-events" className="content events">
          <button onClick={ this.addEvent } className="btn-floating btn-large waves-effect waves-light"><i className="material-icons">add</i></button>
-         { this.getNewEvents() }
          { 
            this.state.titles.map((item, index) => {
              return (

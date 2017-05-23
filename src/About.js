7d1b6e3309b37d6812debdaa44953597ae0a638d
Bálint <you@example.combarcsa.balint@gmail.com>
2017-05-23 11:27:31 +0200

    changes

--- src/About.js
+++ src/About.js
@@ -13,7 +13,7 @@ class About extends Component {
       <div className="card">
         <div className="card-title"><h2>About us</h2></div>
           <div className="card-content">
-            <iframe src="https://www.youtube.com/embed/4gSOMba1UdM" frameBorder="0" allowFullScreen></iframe>
+            <iframe src="https://www.youtube.com/embed/G3QnWI4p9r8" frameborder="0" allowfullscreen></iframe>
             <p>You probably haven't heard of them tote bag knausgaard hoodie hell of. Vinyl tbh woke, hexagon mumblecore coloring book migas listicle. Tousled migas subway tile asymmetrical fap, pinterest green juice offal selfies master cleanse fam +1 cronut. Chicharrones sriracha tousled thundercats succulents drinking vinegar, hella gentrify ennui street art cornhole tilde. Kale chips enamel pin swag PBR&B, drinking vinegar green juice vexillologist lyft YOLO hella bicycle rights glossier squid echo park flannel. Echo park put a bird on it ugh deep v, lumbersexual DIY flannel microdosing tacos roof party woke jianbing you probably haven't heard of them salvia cronut. Kinfolk af pok pok .</p>
         </div>
       </div>
